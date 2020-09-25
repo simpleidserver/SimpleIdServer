f46a222b6c5d6c337242cd7305f377fb2ea4bfbd
habart thierry <habarthierry@hotmail.fr>
2020-09-25 13:46:19 +0200

    Ticket #48 : Rollback changes

--- src/Scim/SimpleIdServer.Scim/Extensions/AuthorizationOptionsExtensions.cs
+++ src/Scim/SimpleIdServer.Scim/Extensions/AuthorizationOptionsExtensions.cs
@@ -8,19 +8,12 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static void AddDefaultSCIMAuthorizationPolicy(this AuthorizationOptions opts)
         {
-            opts.AddPolicy("QueryScimResource", p => p.RequireAssertion(_ => true));
-            opts.AddPolicy("AddScimResource", p => p.RequireAssertion(_ => true));
-            opts.AddPolicy("DeleteScimResource", p => p.RequireAssertion(_ => true));
-            opts.AddPolicy("UpdateScimResource", p => p.RequireAssertion(_ => true));
-            opts.AddPolicy("BulkScimResource", p => p.RequireAssertion(_ => true));
-            opts.AddPolicy("UserAuthenticated", p => p.RequireAssertion(_ => true));
-
-            // opts.AddPolicy("QueryScimResource", p => p.RequireClaim("scope", "query_scim_resource"));
-            // opts.AddPolicy("AddScimResource", p => p.RequireClaim("scope", "add_scim_resource"));
-            // opts.AddPolicy("DeleteScimResource", p => p.RequireClaim("scope", "delete_scim_resource"));
-            // opts.AddPolicy("UpdateScimResource", p => p.RequireClaim("scope", "update_scim_resource"));
-            // opts.AddPolicy("BulkScimResource", p => p.RequireClaim("scope", "bulk_scim_resource"));
-            // opts.AddPolicy("UserAuthenticated", p => p.RequireAuthenticatedUser());
+            opts.AddPolicy("QueryScimResource", p => p.RequireClaim("scope", "query_scim_resource"));
+            opts.AddPolicy("AddScimResource", p => p.RequireClaim("scope", "add_scim_resource"));
+            opts.AddPolicy("DeleteScimResource", p => p.RequireClaim("scope", "delete_scim_resource"));
+            opts.AddPolicy("UpdateScimResource", p => p.RequireClaim("scope", "update_scim_resource"));
+            opts.AddPolicy("BulkScimResource", p => p.RequireClaim("scope", "bulk_scim_resource"));
+            opts.AddPolicy("UserAuthenticated", p => p.RequireAuthenticatedUser());
         }
     }
 }
