7b7dd64583064fe19a761bc1978dfdd19de7e305
thabart <habarthierry@hotmail.fr>
2023-03-02 16:43:48 +0100

    Ticket #405 : Upgrade EFCore.BulkExtensions

---
+++ src/Scim/SimpleIdServer.Scim.Postgre.Startup/Properties/Consumers/IntegrationEventConsumer.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+using SimpleIdServer.Scim.ExternalEvents;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SimpleIdServer.Scim.SqlServer.Startup.Consumers
+{
+    public class IntegrationEventConsumer : IConsumer<RepresentationRefAttributeAddedEvent>
+    {
+        public Task Consume(ConsumeContext<RepresentationRefAttributeAddedEvent> context)
+        {
+            Debug.WriteLine(context.Message.Id);
+            return Task.CompletedTask;
+        }
+    }
+}
