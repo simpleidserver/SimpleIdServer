0be70481035ba87a69709fcfe5ee38af1b8fc164
thabart <habarthierry@hotmail.fr>
2023-07-05 22:39:38 +0200

    Fix docusaurus link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -102,7 +102,7 @@ const config = {
             items: [
               {
                 label: 'Documentation',
-                to: '/docs/intro',
+                to: '/docs/installation',
               },
             ],
           },
