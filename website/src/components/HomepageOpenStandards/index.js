efe08bbac01c41e08b33184fcf7935d4499cb6aa
thabart <habarthierry@hotmail.fr>
2023-12-12 21:25:01 +0100

    Prepare release 4.6

--- website/src/components/HomepageOpenStandards/index.js
+++ website/src/components/HomepageOpenStandards/index.js
@@ -17,7 +17,7 @@ export default function HomepageOpenStandards() {
                     </div>
                     <div className="text--center padding-horiz--md">
                         <h3>OPENID</h3>
-                        <p>OpenID is a popular federated identity standard for web-based applications.</p>
+                        <p>OpenID is a widely used federated identity standard for web-based applications.</p>
                     </div>
                 </div>
                 <div className="col col--6">
@@ -26,7 +26,7 @@ export default function HomepageOpenStandards() {
                     </div>
                     <div className="text--center padding-horiz--md">
                         <h3>OAUTH</h3>
-                        <p>OAuth is used to authenticate a person using a mobile device, or to authenticate software–identity is not just for humans</p>
+                        <p>OAuth is utilized to authenticate an individual using a mobile device or to authenticate software—identity is not exclusive to humans.</p>
                     </div>
                 </div>
                 <div className="col col--6">
@@ -35,7 +35,7 @@ export default function HomepageOpenStandards() {
                     </div>
                     <div className="text--center padding-horiz--md">
                         <h3>FIDO</h3>
-                        <p>A set of standards that enable the hardware, operating system, browser, and identity provider to authenticate a person using phishing-resistent, client-side biometrics.</p>
+                        <p>A set of standards enables the hardware, operating system, browser, and identity provider to authenticate a person using phishing-resistant, client-side biometrics.</p>
                     </div>
                 </div>
                 <div className="col col--6">
@@ -44,7 +44,7 @@ export default function HomepageOpenStandards() {
                     </div>
                     <div className="text--center padding-horiz--md">
                         <h3>SAML</h3>
-                        <p>SAML, the XML federation ancestor of OpenID Connect, is used primiarily by SaaS and older web-based enterprise applications for SSO.</p>
+                        <p>SAML, the XML federation predecessor of OpenID Connect, is primarily used by SaaS and older web-based enterprise applications for Single Sign-On (SSO).</p>
                     </div>
                 </div>
                 <div className="col col--12">
@@ -53,7 +53,7 @@ export default function HomepageOpenStandards() {
                     </div>
                     <div className="text--center padding-horiz--md">
                         <h3>SCIM2.0</h3>
-                        <p>The SCIM protocol is an application-level REST protocol for provisioning and managing identity data on the web. </p>
+                        <p>The SCIM protocol is an application-level REST protocol for provisioning and managing identity data on the web.</p>
                     </div>
                 </div>
             </div>
