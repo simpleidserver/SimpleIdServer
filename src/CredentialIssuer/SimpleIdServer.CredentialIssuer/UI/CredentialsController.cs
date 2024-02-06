e262f412d8030c380f5bde35401fcc87ed9f5493
thabart <habarthierry@hotmail.fr>
2024-02-06 20:30:00 +0100

    WIP

---
+++ src/CredentialIssuer/SimpleIdServer.CredentialIssuer/UI/CredentialsController.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) SimpleIdServer. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimpleIdServer.CredentialIssuer.Store;
+using SimpleIdServer.CredentialIssuer.UI.ViewModels;
+using System.Threading.Tasks;
+
+namespace SimpleIdServer.CredentialIssuer.UI;
+
+[Authorize("Authenticated")]
+public class CredentialsController : Controller
+{
+    private readonly ICredentialConfigurationStore _credentialConfigurationStore;
+    private readonly ICredentialStore _credentialStore;
+
+    public CredentialsController(
+        ICredentialConfigurationStore credentialConfigurationStore,
+        ICredentialStore credentialStore)
+    {
+        _credentialConfigurationStore = credentialConfigurationStore;
+        _credentialStore = credentialStore;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        // list of credentials...
+        return View(new CredentialsViewModel
+        {
+
+        });
+    }
+
+    public async Task<IActionResult> Share(string configurationId)
+    {
+        // generate the QR code.
+        return null;
+    }
+}
