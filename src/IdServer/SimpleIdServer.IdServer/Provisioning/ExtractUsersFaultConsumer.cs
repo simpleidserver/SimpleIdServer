1339d7b9a269c9068be8873ac10b2afe5dc4c9c9
thabart <habarthierry@hotmail.fr>
2024-06-03 12:48:07 +0200

    WIP

---
+++ src/IdServer/SimpleIdServer.IdServer/Provisioning/ExtractUsersFaultConsumer.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) SimpleIdServer. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using MassTransit;
+using System.Threading.Tasks;
+
+namespace SimpleIdServer.IdServer.Provisioning
+{
+    public class ExtractUsersFaultConsumer : IConsumer<Fault<ExtractUsersCommand>>
+    {
+        public ExtractUsersFaultConsumer()
+        {
+            
+        }
+
+        public Task Consume(ConsumeContext<Fault<ExtractUsersCommand>> context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
