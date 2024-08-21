7e088f273222d6f6bd56d298e5a60aee93b53140
thabart <habarthierry@hotmail.fr>
2024-08-21 16:40:10 +0200

    Try to receive intent data (IOS)

--- src/Mobile/SimpleIdServer.Mobile/Platforms/iOS/AppDelegate.cs
+++ src/Mobile/SimpleIdServer.Mobile/Platforms/iOS/AppDelegate.cs
@@ -1,9 +1,26 @@
 ﻿using Foundation;
+using SimpleIdServer.Mobile.Services;
 
 namespace SimpleIdServer.Mobile;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp(); 
+    
+    [Export("application:continueUserActivity:restorationHandler:")]
+    public override bool ContinueUserActivity(UIKit.UIApplication application, NSUserActivity userActivity, UIKit.UIApplicationRestorationHandler completionHandler)
+    {
+        if (userActivity != null)
+        {
+            var url = userActivity.WebPageUrl?.ToString();
+            if (string.IsNullOrWhiteSpace(url) || !url.StartsWith("openid-credential-offer")) return true;
+            Uri? uri = null;
+            if (!Uri.TryCreate(url, UriKind.Absolute, out uri)) return true;
+            var queries = uri.Query.TrimStart('?').Split('&').Select(t => t.Split('=')).ToDictionary(r => r[0], r => r[1]);
+            CredentialOfferListener.New().Receive(queries);
+        }
+
+        return true;
+    }
 }
