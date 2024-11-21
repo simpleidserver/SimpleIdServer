29c3734a80cc3662c470c48343f555275549eb88
thabart <habarthierry@hotmail.fr>
2024-11-21 21:43:17 +0100

    Fix issue with drang & drop

--- formbuilder/FormBuilder/Components/FormViewerContext.cs
+++ formbuilder/FormBuilder/Components/FormViewerContext.cs
@@ -1,7 +1,40 @@
-﻿namespace FormBuilder.Components;
+﻿using FormBuilder.Models;
+
+namespace FormBuilder.Components;
 
 public class FormViewerContext
 {
+    private Action _droppedCallback;
     public AntiforgeryTokenRecord AntiforgeryToken { get; set; }
-    public IFormElementDefinition SelectedDefinition { get; set; }
+    public IFormElementDefinition SelectedDefinition { get; private set; }
+    public IFormElementRecord SelectedRecord { get; private set; }
+    public SelectionTypes SelectionType { get; private set; }
+
+    public void Set(IFormElementRecord record, Action droppedCallback)
+    {
+        _droppedCallback = droppedCallback;
+        SelectedRecord = record;
+        SelectedDefinition = null;
+        SelectionType = SelectionTypes.RECORD;
+    }
+
+    public void Set(IFormElementDefinition def)
+    {
+        SelectedDefinition = def;
+        SelectedRecord = null;
+        SelectionType = SelectionTypes.DEFINITION;
+    }
+
+    public void DropElement()
+    {
+        SelectedDefinition = null;
+        SelectedRecord = null;
+        if (_droppedCallback != null) _droppedCallback();
+    }
+}
+
+public enum SelectionTypes
+{
+    DEFINITION = 0,
+    RECORD = 1
 }
