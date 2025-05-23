e662839b4f279135ed7c41024c8a948ed11ab8a8
thabart <habarthierry@hotmail.fr>
2025-05-23 14:14:44 +0200

    WIP

---
+++ src/Authzen/SimpleIdServer.Authzen.Startup/Program.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using System.Text.Json;
+
+// See https://aka.ms/new-console-template for more information
+var jsonText = File.ReadAllText("input.json");
+var inputObj = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonText);
+var regoText = File.ReadAllText("sample.rego");
+var inputStream = new AntlrInputStream(regoText);
+var lexer = new RegoLexer(inputStream);
+var tokenStream = new CommonTokenStream(lexer);
+var parser = new RegoParser(tokenStream);
+
+// Le point d'entrée de la grammaire est probablement 'root' (voir RegoParser.g4)
+var tree = parser.root();
+
+// Évaluer la policy Rego sur l'input JSON
+var evaluator = new RegoPolicyEvaluator(inputObj);
+bool isAllowed = evaluator.Visit(tree);
+
+Console.WriteLine($"allow = {isAllowed}");
