eac3bad3379e253420058da8bb8e89568c070dec
Admin <Admin@LENLT>
2026-01-22 22:40:39 +0000

    Add Aspire Support

---
+++ SimpleIdServer.Aspire.Startup/SimpleIdServer.Aspire.Startup.AppHost/AppHost.cs
@@ -0,0 +1,11 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var idserver = builder.AddProject<Projects.SimpleIdServer_IdServer_Template_Startup>("idserver")
+	.WithHttpsEndpoint(5001, name: "idserver-https")
+	.WithHttpEndpoint(5000, name: "idserver-http");
+
+var idserverui = builder.AddProject<Projects.SimpleIdServer_IdServer_Website_Startup>("idserverui");
+
+//var scim = builder.AddProject<Projects.ScimEF>("scim");
+
+builder.Build().Run();
