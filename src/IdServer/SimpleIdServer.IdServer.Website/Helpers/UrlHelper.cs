5d1ffa5d7a4c599e9de2af573c175922e6e4b9a3
thabart <habarthierry@gmail.com>
2026-01-27 12:00:40 +0100

    Ticket #949 : Fix href link in the administration website

--- src/IdServer/SimpleIdServer.IdServer.Website/Helpers/UrlHelper.cs
+++ src/IdServer/SimpleIdServer.IdServer.Website/Helpers/UrlHelper.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) SimpleIdServer. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using SimpleIdServer.IdServer.Helpers;
 
@@ -15,16 +16,27 @@ public class UrlHelper : IUrlHelper
 {
     private readonly IdServerWebsiteOptions _options;
     private readonly IRealmStore _realmStore;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public UrlHelper(IOptions<IdServerWebsiteOptions> options, IRealmStore realmStore)
+    public UrlHelper(
+        IOptions<IdServerWebsiteOptions> options, 
+        IRealmStore realmStore, 
+        IHttpContextAccessor httpContextAccessor)
     {
         _options = options.Value;
         _realmStore = realmStore;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public string GetUrl(string url)
     {
-        if (!_options.IsReamEnabled) return url;
-        return string.IsNullOrWhiteSpace(_realmStore.Realm) ? url : $"/{_realmStore.Realm}{url}";
+        var pathBase = _httpContextAccessor.HttpContext?.Request.PathBase.Value?.TrimEnd('/') ?? string.Empty;        
+        if (!_options.IsReamEnabled)
+        {
+            return string.IsNullOrEmpty(pathBase) ? url : $"{pathBase}{url}";
+        }
+        
+        var realmPath = string.IsNullOrWhiteSpace(_realmStore.Realm) ? string.Empty : $"/{_realmStore.Realm}";
+        return $"{pathBase}{realmPath}{url}";
     }
 }
